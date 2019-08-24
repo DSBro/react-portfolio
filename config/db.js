82d42707f4eccb0a1bd6ef46eeb21986438b934f
Trevor McReynolds <trevor.d.mcreynolds@gmail.com>
2019-08-24 14:32:18 -0700

    add functionality for resume section (experience)

--- config/db.js
+++ config/db.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-mongoose.set('useFindAndModify', false);
 
 // Catch errors before they crash app
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
         useNewUrlParser: true,
-        useCreateIndex: true
+        useCreateIndex: true,
+        useFindAndModify: false
       });
 
     console.log('MongoDB Connected...');
